9db624d821eac076200414b01c4ac436264308a8
Muryllo Pimenta <muryllopimenta@gmail.com>
2020-05-18 15:09:32 -0300

    Removendo funcionalidade inútil, incompatível com o Debian para ler data.

--- covid.js
+++ covid.js
@@ -27,9 +27,8 @@ function covid_api_report_state(uf, pvoid_cb){
  * @param {Date} date 
  */
 function covid_api_report_cases(date, pvoid_cb){
-    let arr = date.toLocaleDateString().split('-');
-    console.log(arr);
-    axios.get(`https://covid19-brazil-api.now.sh/api/report/v1/brazil/${arr[2].toString().padStart(2, '0') + arr[1].toString().padStart(2, '0') + arr[0]}`).then((resp) => { pvoid_cb(resp.data); }).catch((err) => { pvoid_cb(null); });
+    console.log(date.getFullYear().toString().padStart(2, '0') + String(Number(date.getMonth()) + 1).toString().padStart(2, '0') + date.getDay().padStart(2, '0'));
+    axios.get(`https://covid19-brazil-api.now.sh/api/report/v1/brazil/${date.getFullYear().toString().padStart(2, '0') + String(Number(date.getMonth()) + 1).toString().padStart(2, '0') + date.getDay().padStart(2, '0')}`).then((resp) => { pvoid_cb(resp.data); }).catch((err) => { pvoid_cb(null); });
 }
 
 /**
